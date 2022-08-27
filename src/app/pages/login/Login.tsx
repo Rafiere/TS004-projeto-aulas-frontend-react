9de377f917ca039a777392805fff55481bd30a19
Rafael <rafapgbr@gmail.com>
2022-08-27 16:42:52 -0300

    feat: inserindo o hook "useEffect()".

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,9 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Login = () => {
 
+    //Quando qualquer alteração nos inputs for realizada, o React renderizará novamente
+    //esse componente, ou seja, todo o bloco de código que está dentro do
+    //"Login".
+
     const [email, setEmail] = useState(''); //O '' é o valor inicial que será inserido no input.
     const [senha, setSenha] = useState('');
+
+    //Esse bloco de código será executado APENAS quando o componente
+    //"Login" for carregado pela PRIMEIRA VEZ.
+
+    //Se não utilizássemos o "useEffect()", a cada vez que o estado desse componente
+    //fosse alterado, ou seja, a cada caractere que fosse digitado no input, esse bloco de
+    //código seria executado, porém, como queremos que ele seja executado apenas uma
+    //vez, utilizamos o "useEffect()".
+
+    //Isso é muito útil para chamadas de API, pois essas chamadas, na maioria das
+    //vezes, deverão ser realizadas apenas uma única vez.
+    useEffect(() => {
+
+    }, []);
+
+    //Dentro do array, que é o segundo parâmetro do "useEffect()", estamos passando as
+    //dependências, assim, a função que é o primeiro parâmetro do "useEffect()" será
+    //executada apenas quando os valores "email" ou "senha", que foram passados para
+    //dentro do array, sofrerem alterações.
+    useEffect(() => {
+        console.log(email);
+        console.log(senha);
+
+    }, [email, senha]); //Quando qualquer uma das propriedades "email" ou "senha" forem alteradas, essa função será executada.
 
     const handleEntrar = () => {
 
