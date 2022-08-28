828cb4a629f236e05e582d5fc0282a833116785d
Rafael <rafapgbr@gmail.com>
2022-08-28 11:20:55 -0300

    feat: encapsulando todas as rotas com o nome do usuário logado.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Routes} from "./routes";
+import { UsuarioLogadoProvider } from './shared/contexts';
+
+/**
+ * Estamos compartilhando o nome do usuário logado com todas as rotas da
+ * aplicação, assim, através desse provider, podemos obter o nome do usuário
+ * que está logado.
+ */
 
 export const App = () => {
   return (
-      <Routes/>
+      <UsuarioLogadoProvider>
+        <Routes/>
+      </UsuarioLogadoProvider>
   );
 }
 
